29f65b59b5846fd1c4f227bd05e8ed55922cc548
Konton022 <42418105+Konton022@users.noreply.github.com>
2021-10-14 21:57:13 +0500

    task#0 done

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log('HelloWorld!');
+const player1 = {
+  name: "Scorpion",
+  hp: 100,
+  img: "",
+  weapon: ["кувалда", "топор", "пистолет"],
+  attack: function () {
+    console.log(`${this.name} Fight!!!`);
+  },
+};
+
+const player2 = {
+  name: "Sub-Zero",
+  hp: 100,
+  img: "",
+  weapon: ["лед", "мечь", "пулемет"],
+  attack: function () {
+    console.log(`${this.name} Fight!!!`);
+  },
+};
+
+player1.attack();
+player2.attack();
